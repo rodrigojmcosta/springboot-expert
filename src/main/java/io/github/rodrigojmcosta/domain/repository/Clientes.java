d08b841ac7d64e36679586c41a2692ef31e41641
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-15 12:23:42 -0300

    Refatorando operações do CRUD para JpaRepository

--- src/main/java/io/github/rodrigojmcosta/domain/repository/Clientes.java
+++ src/main/java/io/github/rodrigojmcosta/domain/repository/Clientes.java
@@ -2,75 +2,11 @@ package io.github.rodrigojmcosta.domain.repository;
 
 
 import io.github.rodrigojmcosta.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-@SuppressWarnings({"SqlDialectInspection", "SqlNoDataSourceInspection"})
-@Repository
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public Cliente salvar(Cliente cliente) {
-        entityManager.persist(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public Cliente atualizar(Cliente cliente) {
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente) {
-        if (!entityManager.contains(cliente)) {
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id) {
-        deletar(entityManager.find(Cliente.class, id));
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome) {
-        String jpql = " SELECT c FROM Cliente c WHERE c.nome LIKE :nome ";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos() {
-        return entityManager
-                .createQuery("FROM Cliente", Cliente.class)
-                .getResultList();
-    }
-
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                Integer id = resultSet.getInt("id");
-                String nome = resultSet.getString("nome");
-                return new Cliente(id, nome);
-            }
-        };
-    }
-
+    List<Cliente> findByNomeLike(String nome);
 }
