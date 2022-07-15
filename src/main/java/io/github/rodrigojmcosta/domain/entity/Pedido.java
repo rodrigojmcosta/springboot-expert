55ea1f4b7aedf3753d3b12bded42f51c5ebc1848
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-15 12:49:41 -0300

    Mapeando a entidade Pedido

--- src/main/java/io/github/rodrigojmcosta/domain/entity/Pedido.java
+++ src/main/java/io/github/rodrigojmcosta/domain/entity/Pedido.java
@@ -1,13 +1,25 @@
 package io.github.rodrigojmcosta.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "PEDIDO")
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "CLIENTE_ID")
     private Cliente cliente;
+
+    @Column(name = "DATA_PEDIDO")
     private LocalDateTime dataPedido;
+
+    @Column(name = "TOTAL", length = 20, precision = 2)
     private BigDecimal total;
 
     public Integer getId() {
