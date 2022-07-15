92b5590192cb42df9144b7db2491c79d0c4ce9b9
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-15 13:02:52 -0300

    Criando repositórios para as entidades

---
+++ src/main/java/io/github/rodrigojmcosta/domain/repository/ItemsPedido.java
@@ -0,0 +1,6 @@
+package io.github.rodrigojmcosta.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsPedido extends JpaRepository<ItemsPedido, Integer> {
+}
